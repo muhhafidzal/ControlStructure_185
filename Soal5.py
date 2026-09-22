612b21bab6b50d4c66271e178698a06b30a7a196
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-09-22 16:16:57 +0700

    menambahkan tugas soal jawaban no kelima

---
+++ Soal5.py
@@ -0,0 +1,14 @@
+#Write a PYTHON program to produce following design
+#1
+#2 2
+#3 3 3
+#4 4 4 4 
+#5 5 5 5 5
+#If user enters n value as 5
+
+n = int(input("Ente n: "))
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(i, end=" ")
+print()
