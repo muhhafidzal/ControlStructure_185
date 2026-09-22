12a539abf15c4d3080d875f331e54964be22764b
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-09-22 15:34:07 +0700

    menambahkan kondisi nilai

---
+++ Soal1.py
@@ -0,0 +1,7 @@
+# 1.Write a PYTHON program to evaluate the student performance
+#If % is >=90 then Excellent performance
+#If % is >=80 then  Very Good performance
+#If % is >=70 then Good performance
+#If % is >=60 then average performance
+
+nilai = float(input("Masukkan Nilainya:"))
